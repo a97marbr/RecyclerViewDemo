d526195d2cd8d1419d983c7038fae927195fea00
a97marbr <a97marbr@gmail.com>
2022-04-06 08:56:36 +0200

    Read the asset file.

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.recyclerviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String json = loadJSONFromAsset(getApplicationContext());
+        Log.d("==>","DATA:'" + json + "'");
+    }
+
+    public String loadJSONFromAsset(Context context) {
+        String json;
+        try {
+            InputStream is = context.getAssets().open("mountains.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            int c = is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, StandardCharsets.UTF_8);
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
     }
 }
